d65ecaf275aad15e1531fae1fe0871341739bcc8
Pav_lov_sky <infopavlovskiy@gmail.com>
2021-09-01 23:22:43 +0300

    transfer dishes-html to partials

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.scss';
 import './js/menu';
-// import './js/dishes';
+import './js/dishes';
+import './js/nav';
--- src/js/dishes.js
+++ src/js/dishes.js
@@ -1,25 +1,45 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+/* COLOR THEME*/
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 function greenTheme(){
 ctx.fillStyle = "green";
 ctx.fillRect(30, 30, 300, 300);
 }
 greenTheme();
+
 function redTheme(){
 ctx.fillStyle = "red";
 ctx.fillRect(30, 30, 300, 300);
 }
 redTheme();
 
+function blueTheme(){
+  ctx.fillStyle = "blue";
+  ctx.fillRect(30, 30, 300, 300);
+  }
+  blueTheme();
 
+function yellowTheme(){
+  ctx.fillStyle = "yellow";
+  ctx.fillRect(30, 30, 300, 300);
+}
+yellowtheme();  
+
+function blackTheme(){
+  ctx.fillStyle = "black";
+  ctx.fillRect(30, 30, 300, 300);
+}
+blackTheme();
+
+
+/* NEW THEME*/
 function newTheme(){
-    var pattern = Trianglify({
+    const pattern = Trianglify({
       width: window.innerWidth,
       height: window.innerHeight
     });
   
     pattern.canvas(document.getElementById('c'));
   }
-  
   newTheme();
